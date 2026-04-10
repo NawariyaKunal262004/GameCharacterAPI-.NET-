011c870797adf33f6e73cc47e7c9d7d4948ac13f
Kunal Nawariya <146311711+NawariyaKunal262004@users.noreply.github.com>
2026-04-10 21:10:00 +0530

    2nd day Commit

---
+++ VideoGameCharacterApi/Controllers/GameCharacterController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VideoGameCharacterApi.Models;
+using VideoGameCharacterApi.Services;
+
+namespace VideoGameCharacterApi.Controllers;
+
+        [Route("api/[controller]")]
+[ApiController]
+public class GameCharacterController(IVideoGameCharacterService service) : ControllerBase
+{
+
+    [HttpGet]
+    public async Task<ActionResult<List<Character>>> GetCharacters()
+            => Ok(await service.GetAllCharctersAsync());
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Character>> GetCharacter(int id)
+    {
+        var character = await service.GetCharacterByIdAsync(id);
+        return character is null ? NotFound("Character with the given Id was not found.") : Ok(character);
+ 
+    }
+}
+
---
+++ VideoGameCharacterApi/Services/VideoGameCharacterService.cs
@@ -0,0 +1,39 @@
+﻿using VideoGameCharacterApi.Models;
+
+namespace VideoGameCharacterApi.Services
+{
+    public class VideoGameCharacterService : IVideoGameCharacterService
+    {
+        static List<Character> characters = new List<Character>
+        {
+            new Character { Id = 1, Name = "Mario", Game = "Super Mario Bros.", Role = "Hero" },
+            new Character {Id = 2, Name = "Link", Game = "Legend of zelta", Role = "Hero" },
+            new Character {Id = 3, Name = "Bowser", Game = "Super Mario Bros.", Role = "Villain" },
+            new Character {Id = 4, Name = "zelda", Game = "The Legend of zelta", Role = "Princess" },
+        };
+
+        public Task<Character> AddCharacterAsync(Character character)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCharacterAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Character>> GetAllCharctersAsync()
+            => await Task.FromResult(characters);
+
+        public async Task<Character?> GetCharacterByIdAsync(int id)
+        {
+            var result = characters.FirstOrDefault(c => c.Id == id);
+            return await Task.FromResult(result);
+        }
+
+        public Task<bool> UpdateCharacterAsync(int id, Character character)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
